f16b5cc99b2d907749344d2ddf7ed3553cd135a4
Natsukashii <natsukashii@Natsukashii.local>
2017-10-16 22:54:42 +0800

    lab4

---
+++ lab4/linktable.h
@@ -0,0 +1,36 @@
+//
+//  Created by Natsukashii on 2017/9/21.
+//  Copyright © 2017年 Natsukashii. All rights reserved.
+//
+#ifndef _LINK_TABLE_H_
+#define _LINK_TABLE_H_
+#include <pthread.h>
+#define  SUCCESS 0
+#define  FAILURE (-1)
+
+/*
+ * LinkTableNode Node Type
+ */
+typedef struct LinkTableNode
+{
+    struct LinkTableNode *pNext;
+}tLinkTableNode;
+
+/*
+ * LinkTableNode Type
+ */
+typedef struct LinkTable
+{
+    tLinkTableNode * pHead;
+    tLinkTableNode * pTail;
+    int            SumOfNode;
+    //pthread_mutex_t_mutex ;
+}tLinkTable;
+
+tLinkTable * CreateLinkTable();
+int DelLinkTable(tLinkTable* pLinkTable);
+int AddLinkTable(tLinkTable* pLinkTable,tLinkTableNode *pNode);
+int DelLinkTableNode(tLinkTable* pLinkTable,tLinkTableNode *pNode);
+tLinkTableNode * GetLinkTableHead(tLinkTable *pLinkTable);
+tLinkTableNode * GetNextLinkTableNode(tLinkTable* pLinkTable,tLinkTableNode *pNode);
+#endif
